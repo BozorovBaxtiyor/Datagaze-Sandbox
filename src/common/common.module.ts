c85007e81e9719e39f15dc88ffe8d60b6d019713
JamesATom <abduhakimovabdulaziz7@gmail.com>
2025-04-19 00:24:42 +0500

    feat(Initial): Initial commit

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,10 +1,7 @@
 // common.module.ts
 import { Module } from '@nestjs/common';
-import { DateService } from './utils/date.service';
 
 @Module({
     imports: [],
-    providers: [DateService],
-    exports: [DateService],
 })
 export class CommonModule {}
--- src/common/enums/operations.enum.ts
+++
@@ -1,6 +0,0 @@
-// operations.enum.ts
-export enum OperationType {
-    INSTALL = 'install',
-    UNINSTALL = 'uninstall',
-    TRANSFER = 'transfer'
-}
